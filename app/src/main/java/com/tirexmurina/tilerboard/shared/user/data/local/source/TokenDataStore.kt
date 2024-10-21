6efe1fe7c799d19981fbf69453d32266d7e50888
tireks <33616425+tireks@users.noreply.github.com>
2024-10-21 16:15:05 +0700

    token local storage

---
+++ app/src/main/java/com/tirexmurina/tilerboard/shared/user/data/local/source/TokenDataStore.kt
@@ -0,0 +1,42 @@
+package com.tirexmurina.tilerboard.shared.user.data.local.source
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+
+class TokenDataStore(context: Context) {
+
+    companion object{
+        private const val ENCRYPTED_PREFS_NAME = "encrypted_prefs_token"
+        const val LOCAL_TOKEN = "local_token"
+    }
+
+    private val masterKey = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val sharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
+        context,
+        ENCRYPTED_PREFS_NAME,
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    fun getAccessToken(): String? = sharedPreferences.getString(LOCAL_TOKEN, null)
+
+    fun setAccessToken(token: String) {
+        sharedPreferences.edit().putString(LOCAL_TOKEN, token).apply()
+    }
+
+    fun isAccessTokenSaved(): Boolean {
+        val token = getAccessToken()
+        return !token.isNullOrEmpty()
+    }
+
+    fun clearAccessToken() {
+        sharedPreferences.edit().remove(LOCAL_TOKEN).apply()
+    }
+
+}
